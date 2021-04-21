925890aa08b389a17697249deb16b00663877059
yutomorita416 <yutomorita416@gmail.com>
2021-04-21 02:47:53 +0000

    finish 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b5c63baa3bd8b6573961f2818428bd9296827ef67362f0797b753d86cec5a693888e4bd295426db4edb7fe0f50646cd0d1292d63cd12b48f4fbd5066c5e36b7f'
